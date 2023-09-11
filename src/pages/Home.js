6204fd168e49cd9123e8d418cc17fa98907d636f
Jihan Ramadhanti <jihan.ramadhanti@flip.id>
2023-09-11 16:02:43 +0700

    add command

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 const Home = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+
+  useEffect(() => {
+    alanBtn({
+      key: "6981aac1c439beebba78b29f3941352b2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command, category }) => {
+        console.log(category);
+        if (command === "setCounter") {
+          incrementCount();
+        } else if (command === "home") {
+          window.location.replace("/");
+        } else if (command === "goto") {
+          window.location.replace("/read/" + category);
+        }
+      },
+    });
+  }, []);
+
   return (
     <div style={{ marginTop: "5%", marginLeft: "10%" }}>
       <h1>Try AI Assistance with Alan AI in react</h1>
       <h3 pAlign="center" variant="h5" component="h3">
-        Try saying "Open page"
+        Calculate Numbers
       </h3>
 
+      <button onClick={incrementCount}>Click Me! </button>
+      <h3 pAlign="center" variant="h5" component="h3">
+        You clicked {count} times
+      </h3>
+
+      <h3 pAlign="center" variant="h5" component="h3">
+        Try saying "Open page"
+      </h3>
       <div>
         <Link to={"/read/array"} className="p">
           <p variant="h5" component="h5">
